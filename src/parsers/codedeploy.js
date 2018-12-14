e45da38a7ec667cb3e742e3781245e836db7932b
Grey Vugrin <gvugrin@classy.org>
2018-12-14 10:10:25 -0800

    Update codedeploy parser to handle sns, add .idea to gitignore
    Formatting

--- src/parsers/codedeploy.js
+++ src/parsers/codedeploy.js
@@ -7,44 +7,44 @@ const BbPromise = require("bluebird"),
 class CodeDeployParser {
 
 	parse(event) {
-		return BbPromise.try(() => _.isObject(event) ? event : JSON.parse(event))
+		return BbPromise.try(() => JSON.parse(_.get(event, "Records[0].Sns.Message", "{}")))
 		.catch(_.noop) // ignore JSON errors
-		.then(event => {
-			if (_.get(event, "source") !== "aws.codedeploy") {
+		.then(message => {
+			if (!_.has(message, "deploymentId") || !_.has(message, "deploymentGroupName")) {
 				return BbPromise.resolve(false);
 			}
 
-			const time = new Date(_.get(event, "time"));
-			const deployState = _.get(event, "detail.state");
-			const deploymentGroup = _.get(event, "detail.deploymentGroup");
-			const deploymentId = _.get(event, "detail.deploymentId");
-			const app = _.get(event, "detail.application");
-			const statusUrl = `https://console.aws.amazon.com/codedeploy/home?region=${event.region}#/deployments/${deploymentId}`;
+			const time = new Date(_.get(event, "Records[0].Sns.Timestamp"));
+			const deployStatus = _.get(message, "status");
+			const deploymentGroup = _.get(message, "deploymentGroupName");
+			const deploymentId = _.get(message, "deploymentId");
+			const app = _.get(message, "applicationName");
+			const statusUrl = `https://console.aws.amazon.com/codedeploy/home?region=${message.region}#/deployments/${deploymentId}`;
 			const fields = [];
 
 			let color = Slack.COLORS.neutral;
 			const baseTitle = `CodeDeploy Application ${app}`;
 			let title = baseTitle;
-			if (deployState === "SUCCESS") {
+			if (deployStatus === "SUCCEEDED") {
 				title = `<${statusUrl}|${baseTitle}> has finished`;
 				color = Slack.COLORS.ok;
 			}
-			else if (deployState === "STOP") {
+			else if (deployStatus === "STOPPED") {
 				title = `<${statusUrl}|${baseTitle}> was stopped`;
 				color = Slack.COLORS.warning;
 			}
-			else if (deployState === "FAILURE") {
+			else if (deployStatus === "FAILED") {
 				title = `<${statusUrl}|${baseTitle}> has failed`;
 				color = Slack.COLORS.critical;
 			}
-			else if (deployState === "START") {
+			else if (deployStatus === "CREATED") {
 				title = `<${statusUrl}|${baseTitle}> has started deploying`;
 			}
 
-			if (deployState) {
+			if (deployStatus) {
 				fields.push({
 					title: "Status",
-					value: deployState,
+					value: deployStatus,
 					short: true
 				});
 			}
@@ -58,7 +58,7 @@ class CodeDeployParser {
 			const slackMessage = {
 				attachments: [{
 					author_name: "AWS CodeDeploy Notification",
-					fallback: `${baseTitle} ${deployState}`,
+					fallback: `${baseTitle} ${deployStatus}`,
 					color: color,
 					title: title,
 					fields: fields,
