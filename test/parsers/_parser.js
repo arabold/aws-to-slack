3404b465be04a822d08c1df864f16ef32ee54fa4
Jesse Decker <me@jessedecker.com>
2019-01-03 16:09:42 -0800

    abstract away the SNS packaging

--- test/parsers/_parser.js
+++ test/parsers/_parser.js
@@ -1,4 +1,5 @@
 /* global expect */
+/* eslint lodash/prefer-lodash-typecheck:0 */
 
 const Handler = require("../../src/index");
 
@@ -7,6 +8,12 @@ const Handler = require("../../src/index");
  */
 class ParserMock {
 
+	/**
+	 * Created instance for chainable calls.
+	 *
+	 * @param {string} name Name of file within parsers/ namespace
+	 * @returns {ParserMock} newly-created instance
+	 */
 	static named(name) {
 		return new ParserMock(name);
 	}
@@ -18,6 +25,46 @@ class ParserMock {
 	makeNew() {
 		const parser = require(`../../src/parsers/${this.name}`);
 		return new parser();
+	}
+
+	matchesSNS(event) {
+		if (typeof event !== "string") {
+			event = JSON.stringify(event);
+		}
+		function rand(digits) {
+			return String(Math.random() * 100000000).substr(0, digits);
+		}
+		event = {
+			Records: [{
+				"EventVersion": "1.0",
+				"EventSubscriptionArn": "arn:aws:sns:region:9999999991:topicname:subscriptionid",
+				"EventSource": "aws:sns",
+				"Sns": {
+					"SignatureVersion": "1",
+					"Timestamp": `1970-0${rand(1)}-2${rand(1)}T0${rand(1)}:0${rand(1)}:0${rand(1)}.${rand(3)}Z`,
+					"Signature": "EXAMPLE",
+					"SigningCertUrl": "EXAMPLE",
+					"MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+					"Message": event,
+					"MessageAttributes": {
+						"Test": {
+							"Type": "String",
+							"Value": "TestString"
+						},
+						"TestBinary": {
+							"Type": "Binary",
+							"Value": "TestBinary"
+						}
+					},
+					"Type": "Notification",
+					"UnsubscribeUrl": "EXAMPLE",
+					"TopicArn": "arn:aws:sns:region:9999999991:topicname",
+					"Subject": "TestInvoke"
+				}
+			}]
+		};
+
+		return this.matchesEvent(event);
 	}
 
 	matchesEvent(event) {
--- test/parsers/autoscaling.test.js
+++ test/parsers/autoscaling.test.js
@@ -2,30 +2,12 @@
 
 require("./_parser")
 	.named("autoscaling")
-	.matchesEvent({ Records: [{
-		"EventVersion": "1.0",
-		"EventSubscriptionArn": `arn:aws:sns:region:account-id:topicname:subscriptionid`,
-		"EventSource": "aws:sns",
-		"Sns": {
-			"SignatureVersion": "1",
-			"Timestamp": "1970-01-01T00:00:00.000Z",
-			"Signature": "EXAMPLE",
-			"SigningCertUrl": "EXAMPLE",
-			"MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
-			"Message": `{"AccountId":"849571656326","RequestId":"bad2bfb5-0a61-11e9-aed0-a14d1975b70c","AutoScalingGroupARN":"arn:aws:autoscaling:us-east-1:849571656326:autoScalingGroup:d4a75f82-0970-4761-80f8-8ebc4999761d:autoScalingGroupName/revo-ecs-cluster-AutoScalingGroup-6TD45WIULWSY","AutoScalingGroupName":"revo-ecs-cluster-AutoScalingGroup-6TD45WIULWSY","Service":"AWS Auto Scaling","Event":"autoscaling:TEST_NOTIFICATION","Time":"2018-12-28T05:30:47.704Z"}`,
-			"MessageAttributes": {
-				"Test": {
-					"Type": "String",
-					"Value": "TestString"
-				},
-				"TestBinary": {
-					"Type": "Binary",
-					"Value": "TestBinary"
-				}
-			},
-			"Type": "Notification",
-			"UnsubscribeUrl": "EXAMPLE",
-			"TopicArn": `arn:aws:sns:region:account-id:topicname`,
-			"Subject": "TestInvoke"
-		}
-	}] });
+	.matchesSNS({
+		"AccountId": "849571656326",
+		"RequestId": "bad2bfb5-0a61-11e9-aed0-a14d1975b70c",
+		"AutoScalingGroupARN": "arn:aws:autoscaling:us-east-1:849571656326:autoScalingGroup:d4a75f82-0970-4761-80f8-8ebc4999761d:autoScalingGroupName/revo-ecs-cluster-AutoScalingGroup-6TD45WIULWSY",
+		"AutoScalingGroupName": "revo-ecs-cluster-AutoScalingGroup-6TD45WIULWSY",
+		"Service": "AWS Auto Scaling",
+		"Event": "autoscaling:TEST_NOTIFICATION",
+		"Time": "2018-12-28T05:30:47.704Z"
+	});
