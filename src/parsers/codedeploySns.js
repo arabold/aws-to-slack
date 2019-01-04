9cb15a314f027d3d5bf0bfd2c439b16daf859d42
Jesse Decker <me@jessedecker.com>
2019-01-03 17:03:37 -0800

    merge fork-origin PR by @greyvugrin https://github.com/arabold/aws-to-slack/pull/32

---
+++ src/parsers/codedeploySns.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const _ = require("lodash"),
+	SNSParser = require("./sns"),
+	Slack = require("../slack");
+
+class CodeDeploySNSParser extends SNSParser {
+
+	handleMessage(message, record) {
+		if (!_.has(message, "deploymentId") || !_.has(message, "deploymentGroupName")) {
+			return false;
+		}
+
+		const time = new Date(_.get(record, "Sns.Timestamp"));
+		const deployStatus = _.get(message, "status");
+		const deploymentGroup = _.get(message, "deploymentGroupName");
+		const deploymentId = _.get(message, "deploymentId");
+		const app = _.get(message, "applicationName");
+		const statusUrl = `https://console.aws.amazon.com/codedeploy/home?region=${message.region}#/deployments/${deploymentId}`;
+		const fields = [];
+
+		let color = Slack.COLORS.neutral;
+		const baseTitle = `CodeDeploy Application ${app}`;
+		let title = baseTitle;
+		if (deployStatus === "SUCCEEDED") {
+			title = `<${statusUrl}|${baseTitle}> has finished`;
+			color = Slack.COLORS.ok;
+		}
+		else if (deployStatus === "STOPPED") {
+			title = `<${statusUrl}|${baseTitle}> was stopped`;
+			color = Slack.COLORS.warning;
+		}
+		else if (deployStatus === "FAILED") {
+			title = `<${statusUrl}|${baseTitle}> has failed`;
+			color = Slack.COLORS.critical;
+		}
+		else if (deployStatus === "CREATED") {
+			title = `<${statusUrl}|${baseTitle}> has started deploying`;
+		}
+
+		if (deployStatus) {
+			fields.push({
+				title: "Status",
+				value: deployStatus,
+				short: true
+			});
+		}
+
+		fields.push({
+			title: "DeploymentGroup",
+			value: `${deploymentGroup}`,
+			short: true
+		});
+
+		return {
+			attachments: [{
+				author_name: "AWS CodeDeploy Notification",
+				fallback: `${baseTitle} ${deployStatus}`,
+				color: color,
+				title: title,
+				fields: fields,
+				mrkdwn_in: [ "title", "text" ],
+				ts: Slack.toEpochTime(time)
+			}]
+		};
+	}
+}
+
+module.exports = CodeDeploySNSParser;
