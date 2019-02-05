bed25fdbab4f8b550bf6481aca10a6b383113816
lehneres <lehneres@outlook.de>
2019-02-05 07:56:39 +0100

    changed to async

--- src/parsers/batch-events.js
+++ src/parsers/batch-events.js
@@ -1,71 +1,66 @@
 "use strict";
 
-const BbPromise = require("bluebird"),
-	_ = require("lodash"),
-	Slack = require("../slack");
+const _ = require("lodash")
+    , SNSParser = require("./sns")
+    , Slack = require("../slack");
 
-class BatchParser {
+class BatchParser extends SNSParser {
 
-	parse(event) {
-		return BbPromise.try(() => JSON.parse(_.get(event, "Records[0].Sns.Message", "{}")))
-			.catch(_.noop) // ignore JSON errors
-			.then(event => {
-				if (_.get(event, "source") !== "aws.batch") {
-					return BbPromise.resolve(false);
-				}
+    handleMessage(message) {
+        if (_.get(message, "source") !== "aws.batch") {
+            return false;
+        }
 
-				const time = new Date(_.get(event, "time"));
-				const status = _.get(event, "detail.status");
-				const reason = _.get(event, "detail.statusReason");
-				const jobName = _.get(event, "detail.jobName");
-				const logStream = _.get(event, "detail.attempts[0].container.logStreamName");
-				const logsUrl = `https://console.aws.amazon.com/cloudwatch/home?region=${event.region}#logEventViewer:group=/aws/batch/job;stream=${logStream};`;
-				const fields = [];
+        const time = new Date(_.get(message, "time"));
+        const status = _.get(message, "detail.status");
+        const reason = _.get(message, "detail.statusReason");
+        const jobName = _.get(message, "detail.jobName");
+        const logStream = _.get(message, "detail.attempts[0].container.logStreamName");
+        const logsUrl = `https://console.aws.amazon.com/cloudwatch/home?region=${message.region}#logEventViewer:group=/aws/batch/job;stream=${logStream};`;
+        const fields = [];
 
-				let color = Slack.COLORS.neutral;
-				const baseTitle = `Batch Job Event ${jobName}`;
-				let title = baseTitle;
-				if (status === "SUCCESS") {
-					title = `${jobName} succeeded`;
-					color = Slack.COLORS.ok;
-				}
-				else if (status === "FAILED") {
-					title = `${jobName} failed`;
-					color = Slack.COLORS.critical;
-				}
+        let color = Slack.COLORS.neutral;
+        const baseTitle = `Batch Job Event ${jobName}`;
+        let title = baseTitle;
+        if (status === "SUCCESS") {
+            title = `${jobName} succeeded`;
+            color = Slack.COLORS.ok;
+        }
+        else if (status === "FAILED") {
+            title = `${jobName} failed`;
+            color = Slack.COLORS.critical;
+        }
 
-				fields.push({
-					title: "Status",
-					value: status,
-					short: true
-				});
+        fields.push({
+            title: "Status",
+            value: status,
+            short: true
+        });
 
-				fields.push({
-					title: "Reason",
-					value: `${reason}`,
-					short: true
-				});
+        fields.push({
+            title: "Reason",
+            value: `${reason}`,
+            short: true
+        });
 
-				fields.push({
-					title: "Logs",
-					value: `<${logsUrl}|View Logs>`,
-					short: true
-				});
+        fields.push({
+            title: "Logs",
+            value: `<${logsUrl}|View Logs>`,
+            short: true
+        });
 
-				const slackMessage = {
-					attachments: [{
-						author_name: "AWS Batch Notification",
-						fallback: `${baseTitle} ${status}`,
-						color: color,
-						title: title,
-						fields: fields,
-						mrkdwn_in: [ "title", "text" ],
-						ts: Slack.toEpochTime(time)
-					}]
-				};
-				return BbPromise.resolve(slackMessage);
-			});
-	}
+        return {
+            attachments: [{
+                author_name: "AWS Batch Notification",
+                fallback: `${baseTitle} ${status}`,
+                color: color,
+                title: title,
+                fields: fields,
+                mrkdwn_in: [ "title", "text" ],
+                ts: Slack.toEpochTime(time)
+            }]
+        };
+    }
 }
 
 module.exports = BatchParser;
